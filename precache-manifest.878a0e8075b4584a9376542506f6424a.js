a4b57ff6bdb185c09a6b029b03688b13a9ce3bc1
ibrahim-t <ibrahimt.khaleel@gmail.com>
2020-08-31 08:37:42 -0700

    Updates

---
+++ precache-manifest.878a0e8075b4584a9376542506f6424a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0974c0bc2d024cba7130785ff3472631",
+    "url": "/space-x/index.html"
+  },
+  {
+    "revision": "dd5ae098805e4b8fa526",
+    "url": "/space-x/static/css/main.43a3123a.chunk.css"
+  },
+  {
+    "revision": "7454a8fe8020ec0ccb61",
+    "url": "/space-x/static/js/2.4d78d71c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/space-x/static/js/2.4d78d71c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd5ae098805e4b8fa526",
+    "url": "/space-x/static/js/main.b36fa265.chunk.js"
+  },
+  {
+    "revision": "15f0d24e697b0ca28e04",
+    "url": "/space-x/static/js/runtime-main.3a57a70f.js"
+  }
+]);
